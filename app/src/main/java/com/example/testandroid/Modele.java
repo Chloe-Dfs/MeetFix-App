2e312b392099e5916fadde47ab59a8a5a881ba83
Karpa_Dor <chloe.defosse.contact@gmail.com>
2024-05-13 20:26:12 +0200

    ajout fragment + début carousel

--- app/src/main/java/com/example/testandroid/Modele.java
+++ app/src/main/java/com/example/testandroid/Modele.java
@@ -1,8 +1,28 @@
 package com.example.testandroid;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 //classe Modele
 public class Modele {
 
     //mettre toutes les données et méthodes, les listes....
+    private List<Activite> activites;
+    public Modele(){
+        activites = new ArrayList<>();
+        initActivite(); //ajouter activites à la liste
+    }
+
+    private void initActivite(){
+        Activite act1 = new Activite("Réparer Ordinateur", "Vannes" );
+        Activite act2 = new Activite("Upcycling vêtement", "Auray");
+
+        activites.add(act1);
+        activites.add(act2);
+    }
+
+    public List<Activite> getActivites() {
+        return activites;
+    }
 }
